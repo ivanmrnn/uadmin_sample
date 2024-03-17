9e23b8b599c85690691d3b171390266ce1ca9698
unknown <fredericmaranan@gmail.com>
2024-03-17 22:54:28 +0800

    changed if else to switch

--- student_information/models/IDFormat.go
+++ student_information/models/IDFormat.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"fmt"
 	"strings"
 
 	"github.com/uadmin/uadmin"
@@ -28,77 +27,48 @@ func (f *IDFormat) Save() {
 		characters = append(characters, string(formatting[i]))
 	}
 	for len(characters) > 0 {
-		for i := 0; i < len(characters); i++ {
-			if characters[i] == "Y" {
-				if i+3 < len(characters) && characters[i+1] == "Y" && characters[i+2] == "Y" && characters[i+3] == "Y" {
-					formatted += "YYYY"
-					characters = append(characters[:i], characters[i+4:]...)
-					break
-				} else if i+2 < len(characters) && characters[i+1] == "Y" && characters[i+2] == "Y" {
-					formatted += "YY"
-					characters = append(characters[:i], characters[i+3:]...)
-					break
-				} else if i+1 < len(characters) && characters[i+1] == "Y" {
-					formatted += "YY"
-					characters = append(characters[:i], characters[i+2:]...)
-					break
-				}
-				characters = append(characters[:i], characters[i+1:]...)
-				break
-			} else if characters[i] == "C" {
-				if i+1 < len(characters) && characters[i+1] == "C" {
-					formatted += "CC"
-					characters = append(characters[:i], characters[i+2:]...)
-					break
-				}
-				characters = append(characters[:i], characters[i+1:]...)
-				break
-			} else if characters[i] == "S" {
-				if i+1 < len(characters) && characters[i+1] == "S" {
-					formatted += "SS"
-					characters = append(characters[:i], characters[i+2:]...)
-					break
-				}
-				characters = append(characters[:i], characters[i+1:]...)
-				break
-			} else if characters[i] == "R" {
-				rCount := 1
-				for i+rCount < len(characters) && characters[i+rCount] == "R" {
-					rCount++
-				}
-				formatted += fmt.Sprintf("%s", strings.Repeat("R", rCount))
-				characters = append(characters[:i], characters[i+rCount:]...)
-				break
-			} else if characters[i] == "N" {
-				rCount := 1
-				for i+rCount < len(characters) && characters[i+rCount] == "N" {
-					rCount++
-				}
-				formatted += fmt.Sprintf("%s", strings.Repeat("N", rCount))
-				characters = append(characters[:i], characters[i+rCount:]...)
-				break
-			} else if characters[i] == "-" {
-				rCount := 1
-				for i+rCount < len(characters) && characters[i+rCount] == "-" {
-					rCount++
-				}
-				formatted += fmt.Sprintf("%s", strings.Repeat("-", rCount))
-				characters = append(characters[:i], characters[i+rCount:]...)
-				break
-			} else if characters[i] == " " {
-				rCount := 1
-				for i+rCount < len(characters) && characters[i+rCount] == " " {
-					rCount++
-				}
-				formatted += fmt.Sprintf("%s", strings.Repeat(" ", rCount))
-				characters = append(characters[:i], characters[i+rCount:]...)
-				break
-			} else {
-				// If the character is not in the specified formats, add a default format
-				formatted = "YY-RRRRR"
-				characters = []string{}
-				break
+		switch characters[0] {
+		case "Y":
+			switch {
+			case len(characters) >= 4 && characters[1] == "Y" && characters[2] == "Y" && characters[3] == "Y":
+				formatted += "YYYY"
+				characters = characters[4:]
+			case len(characters) >= 3 && characters[1] == "Y" && characters[2] == "Y":
+				formatted += "YY"
+				characters = characters[3:]
+			case len(characters) >= 2 && characters[1] == "Y":
+				formatted += "YY"
+				characters = characters[2:]
+			default:
+				formatted += "Y"
+				characters = characters[1:]
 			}
+		case "C":
+			if len(characters) >= 2 && characters[1] == "C" {
+				formatted += "CC"
+				characters = characters[2:]
+			} else {
+				formatted += "C"
+				characters = characters[1:]
+			}
+		case "S":
+			if len(characters) >= 2 && characters[1] == "S" {
+				formatted += "SS"
+				characters = characters[2:]
+			} else {
+				formatted += "S"
+				characters = characters[1:]
+			}
+		case "R", "N", "-", " ":
+			count := 1
+			for count < len(characters) && characters[count] == characters[0] {
+				count++
+			}
+			formatted += strings.Repeat(characters[0], count)
+			characters = characters[count:]
+		default:
+			formatted = "YY-RRRRR"
+			characters = []string{}
 		}
 	}
 	f.Format = formatted
